6ff38b7f7a9d09e38b9359004c09413cf0f23d17
Brillian Esa <brillianesa@yahoo.co.id>
2023-08-22 15:36:29 +0700

    Base project

---
+++ src/main/java/com/gdp/codetest/CodetestApplication.java
@@ -0,0 +1,13 @@
+package com.gdp.codetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodetestApplication.class, args);
+	}
+
+}
